087ab553ddb7cc9d88a3e7f9bce782501d5ddbda
Rafael Costa <orracosta@icloud.com>
2020-09-11 14:51:45 -0300

    Added lootlog

--- AlbionRadar/Player/PlayerItem.cs
+++ AlbionRadar/Player/PlayerItem.cs
@@ -1,93 +1,36 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AlbionNetwork2D
 {
-    class PlayerItem
+    class PlayerLoot
     {
-        public static List<PlayerItem> listItems = new List<PlayerItem>();
+        public static ConcurrentDictionary<int, PlayerLoot> listLoot = new ConcurrentDictionary<int, PlayerLoot>();
+        public static int id = 0;
+        public static bool canAdd = false;
 
-        int id;
-        string uniqueName;
-        string localizedNames;
+        string user;
+        string item;
+        int amount;
 
-        PlayerItem(int id, string uniqueName, string localizedNames)
+        PlayerLoot(string user, string item, int amount)
         {
-            this.id = id;
-            this.uniqueName = uniqueName;
-            this.localizedNames = localizedNames;
+            this.user = user;
+            this.item = item;
+            this.amount = amount;
         }
-        public static void loadItemList()
-        {
-            dynamic jsonArray = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(Properties.Resources.items));
-            foreach (var item in jsonArray)
-            {
-                var localizedName = item["LocalizedNames"] != null ? item["LocalizedNames"]["PT-BR"] : "";
-                listItems.Add(new PlayerItem((int)item["Index"], (string)item["UniqueName"], (string)localizedName));
-                //Console.WriteLine("https://render.albiononline.com/v1/item/" + item["UniqueName"] + ".png?size=80");
-            }
-        }
-        public static PlayerItem getItem(int itemID)
-        {
-            return listItems.FirstOrDefault(i => i.id == itemID);
-        }
-        public static string getLocalizedNameItem(int itemID)
-        {
-            PlayerItem item = listItems.FirstOrDefault(i => i.id == itemID);
 
-            if (item != null)
-                return item.localizedNames;
-            else
-                return "NONE";
-        }
-        public static bool isRanged(int itemID)
+        public string User { get => user; }
+        public string Item { get => item; }
+        public int Amount { get => amount; }
+
+        public static void addLog(string user, string item, int amount)
         {
-            PlayerItem item = listItems.FirstOrDefault(i => i.id == itemID);
-            if (item != null && (item.uniqueName.Contains("BOW") || item.uniqueName.Contains("STAFF")))
-                return true;
-            else
-                return false;
-        }
-        public static bool isTwoHandded(int itemID)
-        {
-            PlayerItem item = listItems.FirstOrDefault(i => i.id == itemID);
-            if (item != null && item.uniqueName.Contains("2H_"))
-                return true;
-            else
-                return false;
-        }
-        public static Image getItemImage(int itemID)
-        {
-            PlayerItem item = listItems.FirstOrDefault(i => i.id == itemID);
-
-            if(item == null || itemID == 0)
-                return new Bitmap(1, 1);
-
-            TextInfo myTI = new CultureInfo("pt-BR", false).TextInfo;
-
-            string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            string strWorkPath = Path.GetDirectoryName(strExeFilePath);
-            var itemImage = Path.Combine(strWorkPath, "Assets\\" + myTI.ToLower(item.uniqueName) + ".png");
-
-            if (File.Exists(itemImage))
-            {
-                return Image.FromFile(itemImage);
-            }
-            else
-            {
-                //System.Net.WebRequest request = System.Net.WebRequest.Create("https://render.albiononline.com/v1/item1/" + item.uniqueName + ".png?size=80");
-                //System.Net.WebResponse response = request.GetResponse();
-                //Stream responseStream = response.GetResponseStream();
-
-                return new Bitmap(1,1);
-            }
+            listLoot.TryAdd(id++, new PlayerLoot(user, item, amount));
         }
     }
 }
